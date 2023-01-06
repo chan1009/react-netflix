d2373ff73f7ee62a8f5157cbbc6a5ffe7ff0d794
chan <75971206+chan1009@users.noreply.github.com>
2023-01-06 19:11:59 +0900

    navigation bar 스크롤 생성

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 export default function Nav() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+  });
   return (
-    <nav>
+    <nav className={`nav ${show && "nav__black"} `}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
