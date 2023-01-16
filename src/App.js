9144d1391f594e26ee11d428a9aaeffe40125205
chan <75971206+chan1009@users.noreply.github.com>
2023-01-16 20:11:26 +0900

    React Router Dom 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { Outlet, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
 
-export default function App() {
+const Layout = () => {
   return (
-    <div className="App">
+    <div>
       <Nav />
-      <Banner />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
+
+export default App;
